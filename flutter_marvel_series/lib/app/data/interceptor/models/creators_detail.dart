95dae6f7f9d90356a26b29b2330a7c40bad36597
María Liliana Jiménez Muñoz <marialiji@gmail.com>
2023-02-27 23:07:03 +0100

    Update creators_detail.dart

--- flutter_marvel_series/lib/app/data/interceptor/models/creators_detail.dart
+++ flutter_marvel_series/lib/app/data/interceptor/models/creators_detail.dart
@@ -3,24 +3,43 @@ import 'package:flutter_marvel_series/app/data/interceptor/models/series.dart';
 class CreatorsDetail {
   int id;
   String fullName;
-  ThumbnailCrerator thumbail;
-  CreatorsDetail({this.id, this.thumbail, this.fullName});
+  ThumbnailCreator thumbnail;
+
+  CreatorsDetail({this.id, this.fullName, this.thumbnail});
 
   CreatorsDetail.fromJson(Map<String, dynamic> json) {
-    id = json['id'];
-    fullName = json['fullName'];
+    id = json['id']??'';
+    fullName = json['fullName']??'';
 
-    thumbail = json['thumbnail'] != null
-        ? ThumbnailCrerator.fromJson(json['thumbnail'])
+    thumbnail = json['thumbnail'] != null
+        ? ThumbnailCreator.fromJson(json['thumbnail'])
         : null;
   }
 
   Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
+    final Map<String, dynamic> data = <String, dynamic>{};
 
-    data['id'] = this.id;
-    data['fullName'] = this.fullName;
+    data['id'] = id;
+    data['fullName'] = fullName;
+    if (thumbnail != null) {
+      data['thumbnail'] = thumbnail.toJson();
+    }
 
     return data;
+  }
+
+  int get creatorId => id;
+  set creatorId(int id) => this.id = id;
+
+  String get creatorName => fullName;
+  set creatorName(String name) => this.fullName = name;
+
+  ThumbnailCreator get creatorThumbnail => thumbnail;
+  set creatorThumbnail(ThumbnailCreator thumbnail) =>
+      this.thumbnail = thumbnail;
+
+  @override
+  String toString() {
+    return 'CreatorsDetail{id: $id, fullName: $fullName, thumbnail: $thumbnail}';
   }
 }
